39a0003167b1a97c9aaca957d8f89d4d697cec5b
Rajath sinha <60216743+Rajathsinha@users.noreply.github.com>
2026-08-11 02:10:13 +0530

    Updated Reveal.tsx

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from 'react';
-import { useTextReveal } from '../hooks/useGsapAnimations';
+import { useTextReveal, useSectionReveal, useStaggerChildren } from '../hooks/useGsapAnimations';
 
 interface RevealTextProps {
   children: string;
